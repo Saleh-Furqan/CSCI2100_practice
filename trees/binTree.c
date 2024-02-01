922e276e7ffab4c4cbb29c5e6fe678664893dbfc
Saleh Furqan <iamsaleh.furqan@gmail.com>
2024-02-01 14:36:06 +0800

    some changes

--- trees/binTree.c
+++ trees/binTree.c
@@ -96,3 +96,58 @@ void printTreeNonRecursive(struct Node* root) {
         current = current->right;
     }
 }
+
+// Function that checks if key is in tree 
+struct Node* searchTree(struct Node* root, int key) {
+    if (root->data == NULL || key == root->data) {
+        return root;
+    }
+    if (key < root->data) {
+        return searchTree(root->left, key);
+    } else {
+    return searchTree(root->right, key);
+    }
+    
+}
+
+struct Node* searchTreeNonRecursive(struct Node* root, int key) {
+    while (root != NULL && key != root->data) {
+        if (key < root->data) {
+            root = root->left;
+        } else {
+            root = root->right;
+        }
+    }
+    return root;
+}
+
+// Function that return minimum key in tree
+struct Node* treeMinimum(struct Node* x) {
+    while (x->right != NULL) {
+        x = x->left;
+    }
+    return x;
+}
+
+// Function that returns maximum key in tree
+struct Node* treeMaximum(struct Node* x) {
+    while (x->right != NULL) {
+        x = x->right;
+    }
+    return x;
+}
+
+// Function that returns successor of node x
+struct Node* treeSuccessor(struct Node* x) {
+    if (x->right != NULL) {
+        return treeMinimum(x->right);
+    } else {
+        struct Node* y = x->parent;
+        while (y != NULL && x == y->right) {
+            x = y;
+            y = y->parent;
+        }
+        return y;
+
+    }
+}
